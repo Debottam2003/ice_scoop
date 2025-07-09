719e68301bfd6d0c2d99fe5609e2f07f9b60c32b
Debottam Kar <debottamkar2003@gmail.com>
2025-07-09 18:28:49 +0530

    day2 work done

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 import pkg from "pg";
 
-const {Pool} = pkg;
+const { Pool } = pkg;
 
 const pool = new Pool({
     connectionString: "postgresql://postgres.actjvcfgpicghbptzaqz:okudera@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres",
