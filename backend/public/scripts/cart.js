ecea830bf2292029464c4e34fa2bddd1423b5559
Shubh-2025 <samratkarmakarnaihati@gmail.com>
2025-07-11 22:24:47 +0530

    cart functionality added

--- backend/public/scripts/cart.js
+++ backend/public/scripts/cart.js
@@ -1,27 +1,68 @@
 let email = localStorage.getItem("userEmail");
 let exp = localStorage.getItem("exp");
 if (exp && Number(exp) <= Date.now()) {
-    // Remove the session markers
-    localStorage.removeItem("userEmail");
-    localStorage.removeItem("exp");
-    window.location.href = "/icescoop/login";
+  // Remove the session markers
+  localStorage.removeItem("userEmail");
+  localStorage.removeItem("exp");
+  window.location.href = "/icescoop/login";
 } else if (!exp & !email) {
-    // window.location.href = "/icescoop/error";
-    alert("login first");
-    window.location.href = "/icescoop/login";
+  // window.location.href = "/icescoop/error";
+  alert("login first");
+  window.location.href = "/icescoop/login";
 } else {
-    document.querySelector("body").style.display = "flex";
+  document.querySelector("body").style.display = "flex";
 }
 
 let cartData = localStorage.getItem("cart");
-if (cartData) {
+let cartItems;
 
+if (cartData) {
+  cartItems = JSON.parse(cartData);
+} else cartItems = [];
+
+
+for (let i = 0; i < cartItems.length; i++) {
+    let cart_item = document.createElement("div");
+    cart_item.id = "cart-item";
+
+    let item_img = document.createElement("img");
+    item_img.id = "item-img";
+    item_img.src = cartItems[i].image;
+
+    let item_name = document.createElement("h2");
+    item_name.id = "item-name";
+    item_name.textContent = cartItems[i].name;
+
+    let item_no = document.createElement("p");
+    item_no.id = "item-no";
+    item_no.textContent = cartItems[i].total;
+
+    let item_quantity = document.createElement("p");
+    item_quantity.id = "item-quantity";
+    item_quantity.textContent = cartItems[i].type;
+
+    let item_cost = document.createElement("p");
+    item_cost.id = "item-cost";
+    item_cost.textContent = "₹" + cartItems[i].price;
+
+    let remove = document.createElement("button");
+    remove.id = "remove-item";
+    remove.textContent = "Remove";
+
+    cart_item.appendChild(item_img);
+    cart_item.appendChild(item_name);
+    cart_item.appendChild(item_no);
+    cart_item.appendChild(item_quantity);
+    cart_item.appendChild(item_cost);
+    cart_item.appendChild(remove);
+
+    document.getElementById("cart-section").appendChild(cart_item);
 }
 
 function Goback() {
-    window.location.href = "/icescoop";
+  window.location.href = "/icescoop";
 }
 
 function GoAcc() {
-    window.location.href = "/icescoop/account";
+  window.location.href = "/icescoop/account";
 }
