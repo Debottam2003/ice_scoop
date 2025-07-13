e0d6ec53477203629bbbe65144cfa1e4c156206e
Shubh-2025 <samratkarmakarnaihati@gmail.com>
2025-07-13 21:55:09 +0530

    manage order customization

--- backend/public/scripts/manageProducts.js
+++ backend/public/scripts/manageProducts.js
@@ -1,3 +1,23 @@
-function goAdmin(){
-    window.location.href='/icescoop/admin';
-};
+function goAdmin() {
+  window.location.href = "/icescoop/admin";
+}
+
+function stockToggle() {
+  let stockArray = document.querySelectorAll(".stock");
+  let displayArray = document.querySelectorAll(".instock");
+
+  for (let i = 0; i < stockArray.length; i++) {
+    stockArray[i].addEventListener("click", () => {
+      if (stockArray[i].classList.contains("d")) {
+        stockArray[i].textContent = "In Stock";
+        stockArray[i].classList.remove("d");
+        displayArray[i].textContent = "No";
+      } else {
+        stockArray[i].textContent = "Out Of Stock";
+        stockArray[i].classList.add("d");
+        displayArray[i].textContent = "Yes";
+      }
+    });
+  }
+}
+stockToggle();
